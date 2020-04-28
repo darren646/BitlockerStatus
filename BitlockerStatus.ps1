a3b7137422b0d015f5ba3ce768b2411d10223777
darren646 <77360229@qq.com>
2020-04-28 14:28:17 +0800

    init

---
+++ BitlockerStatus.ps1
@@ -0,0 +1,45 @@
+﻿Import-Module ActiveDirectory
+$listbitlockerstatus=@("hostname,IsEnableBitlocker")
+$ListOU=@("OU=Romania-PC-Give-Admin,OU=WorkStations,OU=DomainComputers,DC=gsc,DC=huawei,DC=com",
+"OU=Romania-PC-Remove-Admin,OU=WorkStations,OU=DomainComputers,DC=gsc,DC=huawei,DC=com")
+
+$ListOU | ForEach-Object{
+    $OU=$_.ToString()
+
+    Get-ADComputer -Filter 'ObjectClass -eq "computer"' -SearchBase $OU | foreach-object {
+        $Computer = $_.name
+        #Check if the Computer Object exists
+        $Computer_Object = Get-ADComputer -Filter {cn -eq $Computer} -Property msTPM-OwnerInformation, msTPM-TpmInformationForComputer
+        if($Computer_Object -eq $null){
+        Write-Host "Error..."
+        }
+        #Check if the computer object has had a BitLocker Recovery Password
+        $Bitlocker_Object = Get-ADObject -Filter {objectclass -eq 'msFVE-RecoveryInformation'} -SearchBase $Computer_Object.DistinguishedName -Properties 'msFVE-RecoveryPassword' | Select-Object -Last 1
+        if($Bitlocker_Object.'msFVE-RecoveryPassword'){
+        $BitLocker_Key = $BitLocker_Object.'msFVE-RecoveryPassword' -imatch 1
+        }
+        else
+        {
+        $BitLocker_Key = "none"
+        }
+
+        #Display Output
+        $strToReport = $Computer +','+ $BitLocker_Key
+        Write-Host $strToReport 
+
+        $listbitlockerstatus+=$strToReport
+
+    }
+
+}
+
+
+
+
+Write-Host "Total computers is "
+Write-Host $listbitlockerstatus.Count
+$now=Get-Date
+$now.ToString("yyyyMMdd")
+$outfile="C:\workstsstatus\Bitlocker gv status GSC Romania "+$now.ToString("yyyyMMdd")+".csv"
+$listbitlockerstatus |Out-File -Encoding utf8 $outfile #-append
+
---
+++ SendEmails.vbs
@@ -0,0 +1,34 @@
+﻿'Send Log File
+Set objMessage = CreateObject("CDO.Message") 
+objMessage.Subject = "[Compliance] GSC Romaina Bitlocker Checking" 
+objMessage.From = "admin@gsc.huawei.com" 
+objMessage.To = "adrian.iosub@huawei.com;baluta.cosmin@huawei.com;victor.pulca@huawei.com;dan.toda@huawei.com;liyongling@huawei.com;gscithelpdesk@huawei.com;yanyouhong@huawei.com;sircgsc@huawei.com" 
+objMessage.TextBody = "GSC Romaina Bitlocker Checking."
+ystr=year(Now)
+mstr=Month(Now)
+if len(mstr)<2 then mstr="0"&mstr
+dstr=day(Now)
+if len(dstr)<2 then dstr="0"&dstr
+dateml=ystr&mstr&dstr
+objMessage.AddAttachment "C:\workstsstatus\Bitlocker gv status GSC Romania "&dateml&".csv"
+'msgbox("C:\workstsstatus\GSC Romania Bitlock gv status"&dateml&".csv")
+ 
+'==This section provides the configuration information for the remote SMTP server.
+'==Normally you will only change the server name or IP.
+objMessage.Configuration.Fields.Item _
+("http://schemas.microsoft.com/cdo/configuration/sendusing") = 2 
+ 
+'Name or IP of Remote SMTP Server
+objMessage.Configuration.Fields.Item _
+("http://schemas.microsoft.com/cdo/configuration/smtpserver") = "172.16.88.144"
+ 
+'Server port (typically 25)
+objMessage.Configuration.Fields.Item _
+("http://schemas.microsoft.com/cdo/configuration/smtpserverport") = 25 
+ 
+objMessage.Configuration.Fields.Update
+ 
+'==End remote SMTP server configuration section==
+ 
+objMessage.Send
+
